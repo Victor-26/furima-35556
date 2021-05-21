f0b7a4200c9e21b14680cadf264a74cde660e81e
Victor-26 <katsar26.gm@gmail.com>
2021-05-21 19:53:14 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,31 @@ class Item < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to :user
+  has_one_attached :image
 
-belongs_to :user
-has_one_attached :image
+  with_options presence: true do
+    validates :category_id,:condition_id,:which_pay_id,:prefecture_id,:period_id,:price,:image
+    end 
+  
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :condition_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :which_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :period_id, numericality: { other_than: 0, message: "can't be blank" }
+    
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}  
+    validates :price,format: { with: /\A[0-9０-９]+\z/, message: 'は半角で入力して下さい。'}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :prefecture
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :which_pay
+    belongs_to :period
+  
+
+  
 
 
          
