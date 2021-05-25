6e5c1f42c77a2af95891a97bd0eccb33bd9f7c0e
Victor-26 <katsar26.gm@gmail.com>
2021-05-25 12:26:36 +0900

    重複していたテストコードを訂正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -51,7 +51,7 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Prefecture can't be blank")
         end  
 
-        it "発送元地域が選択されていないとと登録できない" do
+        it "発送までの日数が選択されていないとと登録できない" do
           @item.period_id = '' 
           @item.valid?
           expect(@item.errors.full_messages).to include("Period can't be blank")
