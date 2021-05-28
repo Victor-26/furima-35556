5bf2cd2651a2d0e636037d8b057f9fb714f5b5ba
Victor-26 <katsar26.gm@gmail.com>
2021-05-28 17:58:16 +0900

    マイグレーションファイルの編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_080421) do
+ActiveRecord::Schema.define(version: 2021_05_28_084141) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 2021_05_24_080421) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "delivery_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postcode", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city_etc", null: false
+    t.string "place_num", null: false
+    t.string "building_name", null: false
+    t.bigint "purchase_info_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchase_info_id"], name: "index_delivery_addresses_on_purchase_info_id"
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "explanation", null: false
@@ -46,6 +58,15 @@ ActiveRecord::Schema.define(version: 2021_05_24_080421) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "purchase_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_purchase_infos_on_item_id"
+    t.index ["user_id"], name: "index_purchase_infos_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -67,5 +88,8 @@ ActiveRecord::Schema.define(version: 2021_05_24_080421) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "delivery_addresses", "purchase_infos"
   add_foreign_key "items", "users"
+  add_foreign_key "purchase_infos", "items"
+  add_foreign_key "purchase_infos", "users"
 end
