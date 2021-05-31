4acc87a56e126c3bea9200fa0a75b05992ff3b85
Victor-26 <katsar26.gm@gmail.com>
2021-05-31 15:32:05 +0900

    tokenの値もpurchase_infoモデルで取り扱えるようにした

--- app/models/combine.rb
+++ app/models/combine.rb
@@ -1,6 +1,6 @@
 class Combine
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id , :city_etc, :place_num , :building_name, :phone_num , :user_id, :item_id
+  attr_accessor :postcode, :prefecture_id , :city_etc, :place_num , :building_name, :phone_num , :user_id, :item_id,:token
 
   with_options presence: true do
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "には-(ハイフン)が必要です"}
@@ -10,6 +10,7 @@ class Combine
     validates :user_id
     validates :item_id
     validates :phone_num,format: { with: /\A\d{10,11}\z/, message: 'は半角11桁以内で入力して下さい。'}
+    validates :token
   end  
 
   def save
