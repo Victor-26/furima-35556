66f0f8098211e3114da33a662f2677d27bf86efe
Victor-26 <katsar26.gm@gmail.com>
2021-06-01 11:36:18 +0900

    外部キーをテストコードのbefore内でユーザーや商品のFactoryBotから生成

--- spec/factories/combines.rb
+++ spec/factories/combines.rb
@@ -6,8 +6,6 @@ FactoryBot.define do
     place_num { '1-1' }
     building_name { '大阪ハイツ' }
     phone_num { '09045693228' }
-    user_id   {1}
-    item_id   {1}
     token {"tok_abcdefghijk00000000000000000"}
     
 
--- spec/models/combine_spec.rb
+++ spec/models/combine_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Combine, type: :model do
   describe '商品購入' do
       before do
-        @combine = FactoryBot.build(:combine)
+        user = FactoryBot.create(:user)
+        item = FactoryBot.create(:item)
+        @combine = FactoryBot.build(:combine, user_id: user.id , item_id: item.id )
+        sleep 0.1
       end
 
       context '内容に問題ない場合' do
@@ -86,13 +89,6 @@ RSpec.describe Combine, type: :model do
           @combine.valid?
           expect(@combine.errors.full_messages).to include("Item can't be blank")
         end
-
-        
-
-
-
-        
-
 
 
 
